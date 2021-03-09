b6d775bcdbddae47134df7cd0cbd24b0b761f70e
hitk1 <luispaulo.degini@gmail.com>
2021-03-09 00:17:08 -0300

    refactor: Refatarões dos retornos as chamadas

--- lib/rocketpay/accounts/transaction.ex
+++ lib/rocketpay/accounts/transaction.ex
@@ -1,36 +1,9 @@
 defmodule Rocketpay.Accounts.Transaction do
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   alias Ecto.Multi
 
   alias Rocketpay.Accounts.Operation
   alias Rocketpay.Repo
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   def call(%{"from" => from_id, "to" => to_id, "value" => value}) do
     withdraw_params = build_params(from_id, value)
@@ -45,7 +18,7 @@ defmodule Rocketpay.Accounts.Transaction do
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation_name, reason, _changes} -> {:error, reason}
-      {:ok, %{deposit: to_account, withdraw: from_account}} -> {:ok, %{deposit: to_account, withdraw: from_account}}
+      {:ok, %{deposit: to_account, withdraw: from_account}} -> {:ok, TransactionResponse.build(from_account, to_account)}
     end
   end
 
